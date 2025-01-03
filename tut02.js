d5ba416ad134e4abfb99c9e52875c530c9c88cc7
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-03 18:19:41 +0000

    datatype

---
+++ tut02.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as a newer version 
+//alert(3+3)//we are using node js not a browser
+/*
+number=>2to power 53
+bigint
+string=""
+boolean=>true,false
+null it is a standalone value
+undefined =>value not decided/assigned
+symbol =>unique
+
+
+object
+
+
+*/
+console.log(typeof"shivesh");//give the data type typeof
+console.log(typeof undefined);//undefined ayega
+console.log(typeof null);//jb ye krenge to object ayega
