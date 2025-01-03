6ce648aeb3956d82d06067b3f0c2d9c1ccfa1fe0
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-03 18:20:10 +0000

    basic

--- tut01.js
+++ tut01.js
@@ -1,1 +1,10 @@
 console.log("shivesh");
+const accountId=1234;
+/* const can not no reassigned and changed */
+console.log(accountId);
+let accountEmail="shiveshmishrajnp2gmai.com";
+var accountPassword="wdkjc";
+// accountId=123;//not alllowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword]);
+//difference btween var and let is:-in var scope is not declared use let
