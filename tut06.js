bdf815fed05dfd820f98b3893141260656fa83a8
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-03 19:35:29 +0000

    data type

---
+++ tut06.js
@@ -0,0 +1,26 @@
+// different data type
+/*
+1)Primitive -7types:-string,number,boolean,null,undefined,symbol,BigInt
+  
+2)Reference tpe of non primitive :-array,objects,function
+
+
+javascript is dynamically typed because
+In JavaScript, variables are not bound to a specific data type.
+A variable can hold a value of any type, and its type can change during runtime.
+javascript
+You don't need to specify the type of a variable when declaring it. The type is determined based on the value assigned.
+
+
+
+
+
+
+*/
+
+// const value={1,2,3};
+let myobj={
+    name: "shivesh",
+    age:12
+};
+console.log(myobj);
