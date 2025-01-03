99e58e5ae49374a98830550dd9296ac8b4767fea
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-03 18:46:12 +0000

    conversion of datatype

---
+++ tut03.js
@@ -0,0 +1,23 @@
+let score=" ";
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber=Boolean(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//we get nan(not a number)
+/*
+type conversion to number
+"33"=>33
+"33abc"=>NaN
+"true/flase"=>1,0
+
+*/
+/*
+type conversion to boolean
+ 1=>true
+ 0=>false
+ "scsa"=>true
+ ""=>false
+ " "=>true
+
+
+*/
