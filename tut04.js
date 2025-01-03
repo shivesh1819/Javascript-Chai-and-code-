032afac5737971ff1914f9760f6a8153aee631ef
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-03 19:03:43 +0000

    operation

---
+++ tut04.js
@@ -0,0 +1,15 @@
+//operation
+/*
+3+2
+3*3
+3**3(power)
+
+
+*/
+let str1="hello";
+let str2=" shivesh"
+let str3=str1+str2;
+console.log(str3);
+console.log(1+2+2);
+console.log("1"+2+2);//if string appear first then it see other as string and add
+console.log(2+3+"1");//it depend on the string
