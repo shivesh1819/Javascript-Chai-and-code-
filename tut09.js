551aa0d7a2aabdac130cdfdc36bd55c6dacc2621
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-04 18:16:19 +0000

    date and time

---
+++ tut09.js
@@ -0,0 +1,14 @@
+///dates
+// let myDate =new Date("2023-01-19");
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(typeof(myDate));
+
+// let myCreateDate=new Date("01-14-2023");
+// console.log(myCreateDate.toLocaleString());
+// let myTimestamp=Date.now();
+// console.log(myTimestamp);
+
+let newDate=new Date();
