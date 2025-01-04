71708f346ae874ce6da963699e65df32d7446972
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-04 16:06:35 +0000

    string and string method

---
+++ tut07.js
@@ -0,0 +1,11 @@
+//string 
+//how to concatenate 
+// const value=10;
+// console.log(`hello my  name is shivehs ${value}`);
+//stirng created with single or doule quotes work the same 
+
+let x=" jo  hn";
+console.log(x.trim());
+console.log(x.toUpperCase())
+console.log(x.slice(1,2));//not include last positionentered it takes a starting an ending value
+console.log(x.replace("h","s"));
