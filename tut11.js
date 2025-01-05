c674b69cd822bfaa30536e1913c63dd122efee4e
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-05 11:29:30 +0000

    array2

---
+++ tut11.js
@@ -0,0 +1,26 @@
+//////////array 2
+const marvel_heroes=["thor","Ironman","Spiderman"];
+const dc_heroes=["superman","flash","batman"];
+
+
+// marvel_heroes.push(dc_heroes);
+// console.log(marvel_heroes);
+
+
+// const allheroes=marvel_heroes.concat(dc_heroes);
+// console.log(allheroes);
+
+// const allHeroes=[...dc_heroes,...marvel_heroes];
+// console.log(allHeroes);
+
+// const another_array=[1,2,3,[2,3,4],2,4,["shivesh","mishra"]];
+// const real_another_array=another_array.flat();
+// console.log(real_another_array);
+
+
+console.log(Array.isArray("shivesh"));
+console.log(Array.from("shivesh"));
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2,score3));
