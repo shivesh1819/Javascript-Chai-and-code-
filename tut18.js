ada69a34e953e985e629edd6f77de6f6d9dd428b
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-08 19:29:29 +0000

    javascript execution

---
+++ tut18.js
@@ -0,0 +1,17 @@
+///javascript Execution Context
+/*
+
+-global exection format
+-function execution format
+-Eval Execution format
+
+two phases -memory ceartin phase
+-execution phase
+
+
+
+
+
+
+
+*/
