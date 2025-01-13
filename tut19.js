c4f25b6d6d582cbc79ba46feb33dca83f7731f46
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-13 23:52:52 +0000

    if else

---
+++ tut19.js
@@ -0,0 +1,23 @@
+/////conditional sentences
+if (true == (2 == "2")) {
+  console.log("Executed");
+}
+const month = 3;
+switch (month) {
+  case 1:
+    console.log("Jan");
+    break;
+  case 2:
+    console.log("feb");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  default:
+    console.log("fuck off");
+    break;
+}
+//shift+alt+down to copy
