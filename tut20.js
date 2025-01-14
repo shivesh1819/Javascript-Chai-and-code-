7f1db673f2e1135e5ca58b9274a767f4e89558ac
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-14 00:11:11 +0000

    truthy and falsy value

---
+++ tut20.js
@@ -0,0 +1,30 @@
+/*
+
+
+
+falsy value--false,0,-0,BigInt 0n,"",null,undefined,NaN,
+
+truth value-"0","false"," ",[],{},function()
+
+
+
+
+
+
+
+
+
+
+*/
+//Nullish Coalescing Operator(?):null undefined
+// let val1;
+// // val1=5 ?? 10;
+// val1=null??"shivesh";
+// console.log(val1);
+
+//ternairy operator 
+//condition ? true : false 
+
+const iceBurg=90;
+iceBurg>=69 ? console.log("less than 80") : SVGComponentTransferFunctionElement.log("not ok");
+console.log(iceBurg);
