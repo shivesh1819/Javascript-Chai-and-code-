664c349bb195e6d4741c8042e11418cfa72cfecc
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-16 18:46:07 +0000

    loop

--- tut21.js
+++ tut21.js
@@ -7,3 +7,16 @@ for (let i=1;i<=10;i++){
     }
 }
 //break and continue...
+
+let num=0;
+while(num<10){
+    console.log(num);
+    num++;
+}///while loop
+
+//do while loop
+let score =1;
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score<=10);
