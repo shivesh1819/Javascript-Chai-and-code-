40b9c69da31c73ea6d8cd1a72cd1665aeea9a904
shivesh1819 <111443345+shivesh1819@users.noreply.github.com>
2025-01-17 10:56:58 +0000

    for in looop

---
+++ tut23.js
@@ -0,0 +1,13 @@
+// const myObj={
+//     js:"javascript",
+//     cpp:"c++",
+//     rb:"ruby"
+// };
+// for (const key in myObj) {
+//    console.log(`The value of key is ${key} and the value of pair is ${myObj[key]}`);
+// }
+const programming=['a','b','c'];
+for (const num in programming) {
+    console.log(num);
+  
+}
